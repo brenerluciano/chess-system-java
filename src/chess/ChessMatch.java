a959c351d2300b06732eeb2a7b3a1c91e0192efd
Brener Luciano ALves Couto <brenerluciano6514@gmail.com>
2024-07-25 16:55:34 -0300

    Queen (and Pawn bugfix)

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -56,7 +56,7 @@ public class ChessMatch {
         }
         check = (testCheck(opponent(currentPlayer))) ? true : false;
 
-        if(testCheck(opponent(currentPlayer))){
+        if(testCheckMate(opponent(currentPlayer))){
             checkMate = true;
         }
         else {
@@ -152,7 +152,7 @@ public class ChessMatch {
                         Position source = ((ChessPiece)p).getChessPosition().toPosition();
                         Position target = new Position(i, j);
                         Piece capturedPiece = makeMove(source, target);
-                        boolean testCheck = testCheckMate(color);
+                        boolean testCheck = testCheck(color);
                         undoMove(source, target, capturedPiece);
                         if(!testCheck){
                             return false;
@@ -174,6 +174,7 @@ public class ChessMatch {
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
@@ -190,6 +191,7 @@ public class ChessMatch {
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
